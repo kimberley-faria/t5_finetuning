5b0325342542f8edde5e036390c847b5d074564a
Kimberley Faria <kimfaria.985@gmail.com>
2021-03-05 00:44:01 -0500

    system specific config

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
+SYSTEM = 'local'
+
 # For data, not backed up
 SCRATCH_DIR = "/mnt/nfs/scratch1/kfaria"
 
 # For code, imp stuff, backed up
 WORKING_DIR = "/mnt/nfs/work1/mccallum/kfaria"
+
+SETTINGS_DICT = {
+    'gypsum': {
+        'data': SCRATCH_DIR,
+        'root': WORKING_DIR
+    },
+    'local': {
+        'data': "../data",
+        'root': "../"
+    }
+}
+
+SETTINGS = SETTINGS_DICT.get(SYSTEM)
