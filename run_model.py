f60a09a7c03de6b648742476d7951b39da922f59
Kimberley Faria <kimfaria.985@gmail.com>
2021-03-07 17:38:04 -0500

    Refactored path strings, added wandb configuration, added logging, set validation dataset size as configurable

--- run_model.py
+++ run_model.py
@@ -1,17 +1,30 @@
-import datetime
 import glob
+import logging
 import os
 import re
 import sys
 
 import tensorflow as tf
-from tensorboard.plugins.hparams import api as hp
 from transformers import TFT5ForConditionalGeneration, AutoTokenizer
+
+import wandb
+
+logger = logging.getLogger('tensorflow')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 
 
 def _build(pos_files, neg_files, tokenizer, max_len=512):
     pos_inputs, pos_targets = _buil_examples_from_files(pos_files, 'positive', tokenizer, max_len=max_len)
     neg_inputs, neg_targets = _buil_examples_from_files(neg_files, 'negative', tokenizer, max_len=max_len)
+    logger.info("Return all inputs and targets")
     return pos_inputs + neg_inputs, pos_targets + neg_targets
 
 
@@ -22,8 +35,8 @@ def _buil_examples_from_files(files, sentiment, tokenizer, max_len=512):
     REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
     REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
 
-    for path in files:
-        with open(path, 'r') as f:
+    for i, path in enumerate(files):
+        with open(path, 'r', encoding="utf8") as f:
             text = f.read()
 
         line = text.strip()
@@ -44,11 +57,14 @@ def _buil_examples_from_files(files, sentiment, tokenizer, max_len=512):
 
         inputs.append(tokenized_inputs)
         targets.append(tokenized_targets)
+        logger.debug(f"Done processing file {i} of {len(files)} for label - {sentiment}")
 
+    logger.info(f"Return tokenized inputs and targets for label: {sentiment}")
     return inputs, targets
 
 
 def get_dataset(tokenizer, type_path, max_len=512, subset=None):
+    logger.info(f"Get {type_path} dataset of size {subset} (If None, get entire dataset)")
     data_dir = 'aclImdb'
     pos_file_path = os.path.join(data_dir, type_path, 'pos')
     neg_file_path = os.path.join(data_dir, type_path, 'neg')
@@ -61,10 +77,12 @@ def get_dataset(tokenizer, type_path, max_len=512, subset=None):
     else:
         inputs, targets = _build(pos_files, neg_files, tokenizer, max_len=max_len)
 
+    logger.info(f"Process ids and masks for dataset: {type_path}")
     return [get_ids_and_masks(inputs, targets, i) for i in range(len(inputs))]
 
 
 def get_ids_and_masks(inputs, targets, index):
+    logger.debug(f"Split up the ids and attention_masks for inputs and targets for index {index}")
     source_ids = inputs[index]["input_ids"][0]
     target_ids = targets[index]["input_ids"][0]
 
@@ -76,16 +94,19 @@ def get_ids_and_masks(inputs, targets, index):
 
 
 def to_tf_dataset(dataset):
+    logger.info("Get a tf dataset")
     columns = ['input_ids', 'attention_mask', 'labels', 'decoder_attention_mask']
     return_types = {'input_ids': tf.int32, 'attention_mask': tf.int32,
                     'labels': tf.int32, 'decoder_attention_mask': tf.int32, }
     return_shapes = {'input_ids': tf.TensorShape([None]), 'attention_mask': tf.TensorShape([None]),
                      'labels': tf.TensorShape([None]), 'decoder_attention_mask': tf.TensorShape([None])}
     ds = tf.data.Dataset.from_generator(lambda: dataset, return_types, return_shapes)
+    logger.info(f"TF Dataset {ds.__str__()} returned")
     return ds
 
 
 def create_dataset(dataset, cache_path=None, batch_size=4, buffer_size=1000, shuffling=True):
+    logger.info(f"Create dataset of batch:{batch_size}, shuffle: {shuffling}, buffer: {buffer_size}")
     if cache_path is not None:
         dataset = dataset.cache(cache_path)
     if shuffling:
@@ -140,86 +161,107 @@ class FinetunedT5(TFT5ForConditionalGeneration):
         self.compiled_metrics.update_state(y, logits)
         return {m.name: m.result() for m in self.metrics}
 
+class CustomCallback(tf.keras.callbacks.Callback):
+    def __init__(self, step=None):
+        self.epoch = 0
+        self.step = step
+        self.train_batch = 0
+        self.valid_batch = 0
+        logger.info(f"Initialized CustomCallback to epoch: {self.epoch}, step: {step} (If none, running training step),"
+                    f" train_batch: {self.train_batch}, valid_batch: {self.valid_batch}")
 
-def setup_tensorboard(log_dir, steps, run_name):
-    start_profile_batch = steps + 10
-    stop_profile_batch = start_profile_batch + 100
-    profile_range = f"{start_profile_batch},{stop_profile_batch}"
-    log_path = log_dir + f"/{run_name}_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path, histogram_freq=1,
-                                                          update_freq=20, profile_batch=profile_range)
-    checkpoint_filepath = SAVE_PATH + "/" + "T5-{epoch:04d}-{val_loss:.4f}.ckpt"
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filepath,
-        save_weights_only=True,
-        monitor='val_loss',
-        mode='min',
-        save_best_only=True)
-    callbacks = [tensorboard_callback, ]
-    metrics = [tf.keras.metrics.SparseTopKCategoricalAccuracy(name='accuracy')]
+    def on_epoch_end(self, epoch, logs=None):
+        logger.debug(f"End of epoch {epoch} ; got log keys: {list(logs.keys())}")
+        wandb.log(
+            {
+                'epoch': epoch,
+                'accuracy': logs['accuracy'],
+                'loss': logs['loss']
+            }
+        )
+        self.epoch += 1
 
-    return callbacks, metrics
+    def on_train_batch_end(self, batch, logs=None):
+        logger.debug(f"End of batch {self.train_batch} of train; got log keys: {list(logs.keys())}")
+
+        wandb.log(
+            {
+                'train_batch': self.train_batch,
+                'train_accuracy': logs['accuracy'],
+                'train_loss': logs['loss']
+            }
+        )
+        self.train_batch += 1
+
+    def on_test_batch_end(self, batch, logs=None):
+        step = self.step if self.step else 'val'
+
+        logger.debug(f"End of batch {self.valid_batch} of {step}; got log keys: {list(logs.keys())}")
+
+        wandb.log(
+            {
+                'valid_batch': self.valid_batch,
+                f'{step}_accuracy': logs['accuracy'],
+                f'{step}_loss': logs['loss']
+            }
+        )
+        self.valid_batch += 1
 
 
-def train_test_model(hparams, run_name):
-    # For Tensorboard
-
-    steps = hparams['ntrain'] // hparams['batch_size']
-
-    optimizer = tf.keras.optimizers.Adam(hparams['lr'])
+def train_test_model(config, run_name):
+    optimizer = tf.keras.optimizers.Adam(config.lr)
 
     tokenizer = AutoTokenizer.from_pretrained('t5-small')
 
-    callbacks, metrics = setup_tensorboard(f"{LOG_DIR}/hparam_tuning", steps, run_name)
-    callbacks.append(hp.KerasCallback(f"{LOG_DIR}/hparam_tuning", hparams))
+    metrics = [tf.keras.metrics.SparseTopKCategoricalAccuracy(name='accuracy')]
+    callbacks = [CustomCallback()]
 
     # Test for 2, 4, 8, 16, 32, 64, etc.. till 20k
     tf_train_ds = to_tf_dataset(
-        get_dataset(tokenizer, 'train', max_len=hparams['encoder_max_len'], subset=hparams['ntrain']))
+        get_dataset(tokenizer, 'train', max_len=config.encoder_max_len, subset=config.ntrain))
 
     # Always run on entire validation set (5000)
     tf_valid_ds = to_tf_dataset(
-        get_dataset(tokenizer, 'val', max_len=hparams['encoder_max_len'], subset=hparams['ntrain']))
+        get_dataset(tokenizer, 'val', max_len=config.encoder_max_len, subset=config.nvalid))
 
-    tf_test_ds = to_tf_dataset(
-        get_dataset(tokenizer, 'test', max_len=hparams['encoder_max_len'], subset=hparams['ntrain']))
+    train_ds = create_dataset(tf_train_ds, batch_size=config.batch_size, shuffling=True,
+                              cache_path=None, buffer_size=2500)
+    valid_ds = create_dataset(tf_valid_ds, batch_size=config.batch_size, shuffling=True,
+                              cache_path=None, buffer_size=2500)
 
-    train_ds = create_dataset(tf_train_ds, batch_size=hparams['batch_size'], shuffling=True, cache_path=None)
-    valid_ds = create_dataset(tf_valid_ds, batch_size=hparams['batch_size'], shuffling=False, cache_path=None)
-    test_ds = create_dataset(tf_test_ds, batch_size=hparams['batch_size'], shuffling=True, cache_path=None)
-
-    model = SnapthatT5.from_pretrained("t5-small")
+    model = FinetunedT5.from_pretrained("t5-small")
 
     model.compile(optimizer=optimizer, metrics=metrics)
 
-    r = model.fit(train_ds, epochs=25, batch_size=hparams['batch_size'], callbacks=callbacks,
-                  validation_data=valid_ds, validation_batch_size=hparams['batch_size'])
+    model.fit(train_ds, epochs=25, batch_size=config.batch_size, callbacks=callbacks,
+              validation_data=valid_ds, validation_batch_size=config.batch_size)
 
-    # test_accuracy, test_loss = model.evaluate(test_ds)
+    # Evaluate on Test Dataset
+    # tf_test_ds = to_tf_dataset(
+    #     get_dataset(tokenizer, 'test', max_len=config.encoder_max_len, subset=None))
+    # test_ds = create_dataset(tf_test_ds, batch_size=config.batch_size, shuffling=True, cache_path=None)
+    # model.evaluate(test_ds, callbacks=[CustomCallback(step='test')])
 
-    return [r.history[x][0] for x in ['accuracy', 'loss', 'val_accuracy', 'val_loss']]
-
-
-def run(run_dir, hparams, run_name):
-    with tf.summary.create_file_writer(run_dir).as_default():
-        hp.hparams(hparams)  # record the values used in this trial
-        train_accuracy, train_loss, val_accuracy, val_loss = train_test_model(hparams, run_name)
-        tf.summary.scalar('accuracy', train_accuracy, step=1)
-        tf.summary.scalar('loss', train_loss, step=1)
-        tf.summary.scalar('val_accuracy', val_accuracy, step=1)
-        tf.summary.scalar('val_loss', val_loss, step=1)
+    # return [r.history[x][0] for x in ['accuracy', 'loss', 'val_accuracy', 'val_loss']] + [test_accuracy, test_loss]
 
 
-DATA_DIR = "./data"
-LOG_DIR = f"{DATA_DIR}/experiments/t5/logs"
-SAVE_PATH = f"{DATA_DIR}/experiments/t5/models"
+def run(run_dir, config, run_name):
+    train_test_model(config, run_name)
+
+
+DATA_DIR = ".\\data"
+LOG_DIR = f"{DATA_DIR}\\experiments\\t5\\logs"
+SAVE_PATH = f"{DATA_DIR}\\experiments\\t5\\models"
 
 if __name__ == '__main__':
     hparams = {
-        'batch_size': int(sys.argv[1]),
-        'encoder_max_len': int(sys.argv[2]),
-        'ntrain': int(sys.argv[3]),
-        'lr': float(sys.argv[4]),
+        'batch_size': 8,
+        'encoder_max_len': 256,
+        'ntrain': 64,
+        'nvalid': 5000,
+        'lr': 0.001,
     }
-    run_name = f"run-{sys.argv[5]}"
-    run(f"{LOG_DIR}/hparam_tuning/" + run_name, hparams, run_name)
+    wandb.init(project='t5-finetuning', config=hparams)
+    config = wandb.config
+    run_name = f"run-0"
+    run(f"{LOG_DIR}\\hparam_tuning\\{run_name}", config, run_name)
