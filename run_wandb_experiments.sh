3c66ea8cbef64bbbccf26af9bd23b5c2f56f19f8
Kimberley Faria <kimfaria.985@gmail.com>
2021-03-31 01:56:36 -0400

    changed slurm job output dir

--- run_wandb_experiments.sh
+++ run_wandb_experiments.sh
@@ -2,6 +2,7 @@
 #SBATCH --partition=1080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40GB
+#SBATCH -o /mnt/nfs/scratch1/kfaria/slurm-output/slurm-%j.out
 
 export XDG_RUNTIME_DIR=""
 conda activate default
