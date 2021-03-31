5a30386fb033f8c2ea3b0096e40fb12221597b7b
Kimberley Faria <kimfaria.985@gmail.com>
2021-03-31 01:03:40 -0400

    removed prev run dependency setting

--- run_wandb_experiments.sh
+++ run_wandb_experiments.sh
@@ -2,7 +2,6 @@
 #SBATCH --partition=1080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40GB
-#SBATCH -d singleton
 
 export XDG_RUNTIME_DIR=""
 conda activate default
@@ -11,5 +10,5 @@ module list
 
 sweep_id=$1
 
-wandb agent --count 12 $sweep_id --project t5-finetuning --entity kfaria
+wandb agent --count 1 $sweep_id --project t5-finetuning --entity kfaria
 wandb sync --clean
