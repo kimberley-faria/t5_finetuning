460a7bb1a9eb855f166ec2313c8e10ded755efbc
kfaria <faria.kimberley@gmail.com>
2021-04-17 11:07:50 -0400

    updated verify_config.py, formatted sweep*.yaml

--- verify_config.py
+++ verify_config.py
@@ -5,11 +5,13 @@ from config import SYSTEM, LABELS_TYPE, SETTINGS
 if __name__ == '__main__':
     print("****************************************************************************************************")
     settings_json = json.dumps(SETTINGS, indent=4)
-    print(
-        f"\nConfig set to: \nSETTINGS: {settings_json}, \nSYSTEM: {SYSTEM}, \nLABELS_TYPE: {LABELS_TYPE}\n")
-
+    print(f"\nConfig set to: \nSETTINGS: {settings_json}, \nSYSTEM: {SYSTEM}, \nLABELS_TYPE: {LABELS_TYPE}\n")
     print("****************************************************************************************************")
     print("\nrun_wandb_experiments.sh contents:")
     print("----------------------------------")
     print(open(f'{SETTINGS.get("root")}/run_wandb_experiments.sh', "r").read())
+    print("****************************************************************************************************")
+    print("\nsweep.yaml contents:")
+    print("--------------------")
+    print(open(f'{SETTINGS.get("root")}/sweep.yaml', "r").read())
     print("****************************************************************************************************")
