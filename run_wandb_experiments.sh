c9a92c6724319bf0ad8827979a3794b51a1113de
kfaria <faria.kimberley@gmail.com>
2021-04-23 14:23:31 -0400

    run plain jobs

--- run_wandb_experiments.sh
+++ run_wandb_experiments.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#SBATCH --partition=1080ti-long
+#SBATCH --partition=2080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20GB
-#SBATCH -o /mnt/nfs/scratch1/kfaria/slurm-output/slurm-%j-%a.out
-#SBATCH --array=1-6
+#SBATCH -o /mnt/nfs/scratch1/kfaria/slurm-output/slurm-%j.out
 
 export XDG_RUNTIME_DIR=""
 conda activate default
