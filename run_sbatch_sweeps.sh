9c4d35d94e0f38d81ab04fe384f1c45cf9013061
kfaria <faria.kimberley@gmail.com>
2021-04-23 17:53:38 -0400

    updated bash scripts

--- run_sbatch_sweeps.sh
+++ run_sbatch_sweeps.sh
@@ -2,7 +2,8 @@
 
 number_of_jobs=$1
 sweep_id=$2
+partition=$3
 
 for var_name in $(seq 1 $number_of_jobs); do
-  sbatch run_wandb_experiments.sh $sweep_id
+  sbatch run_wandb_experiments.sh $sweep_id --partition=$partition
 done
