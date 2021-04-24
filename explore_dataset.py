96a4a713883c7b5d4aa104ce65a18cac529eeaab
kfaria <faria.kimberley@gmail.com>
2021-04-24 15:35:09 -0400

    set dataset to amazonr_dvd in all sweep configs

--- explore_dataset.py
+++ explore_dataset.py
@@ -82,7 +82,7 @@ def t5_tokenized_examples(fname, max_len=128):
 
 
 if __name__ == '__main__':
-    dataset = "amazonr_books"
+    dataset = "amazonr_dvd"
     training_ds_fpath = TRAINING_DATASET_FNAME.format(dataset_name=dataset, dataset_number=0, dataset_size=4)
     _, _, a = training_ds_fpath.partition(f"{dataset}")
     t5_tokenized_examples(training_ds_fpath)
