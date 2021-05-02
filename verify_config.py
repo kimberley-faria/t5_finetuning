107aacfc5f35d5e99e37e9ea70e2a32a2d2907b9
kfaria <faria.kimberley@gmail.com>
2021-05-02 18:45:31 -0400

    update verify config

--- verify_config.py
+++ verify_config.py
@@ -11,8 +11,13 @@ if __name__ == '__main__':
     print("----------------------------------")
     print(open(f'{SETTINGS.get("root")}/run_wandb_experiments.sh', "r").read())
     print("****************************************************************************************************")
-    for size in [4, 8, 16, 32]:
-        print(f"\nsweep{size}.yaml contents:")
-        print("----------------------")
-        print(open(f'{SETTINGS.get("root")}/sweep{size}.yaml', "r").read())
-        print("****************************************************************************************************")
+    # for size in [4, 8, 16, 32]:
+    #     print(f"\nsweep{size}.yaml contents:")
+    #     print("----------------------")
+    #     print(open(f'{SETTINGS.get("root")}/sweep{size}.yaml', "r").read())
+    #     print("****************************************************************************************************")
+
+    print(f"\nsweep.yaml contents:")
+    print("----------------------")
+    print(open(f'{SETTINGS.get("root")}/sweep.yaml', "r").read())
+    print("****************************************************************************************************")
