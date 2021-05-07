56b7ed1d974493ef016e80d280bbd6871686c904
kfaria <faria.kimberley@gmail.com>
2021-05-07 12:44:35 -0400

    amazon_books_t - clean up data before sending to T5

--- explore_dataset.py
+++ explore_dataset.py
@@ -89,7 +89,7 @@ def t5_tokenized_examples(fname, max_len=256):
 
 
 if __name__ == '__main__':
-    dataset = "scitail_b"
+    dataset = "amazon_books_t"
     training_ds_fpath = TRAINING_DATASET_FNAME.format(dataset_name=dataset, dataset_number=0, dataset_size=4)
     _, _, a = training_ds_fpath.partition(f"{dataset}")
     t5_tokenized_examples(training_ds_fpath)
