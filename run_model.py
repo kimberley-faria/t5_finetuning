d40e7689dd60260b590279ae573650d33abfe512
kfaria <faria.kimberley@gmail.com>
2021-06-06 20:34:45 -0400

    mrpc

--- run_model.py
+++ run_model.py
@@ -70,8 +70,8 @@ def t5_tokenized_examples(fname, max_len=128):
         input_text = f"Sentence 1: {bert_input_text[0].strip()} \nSentence 2: {bert_input_text[1].strip()}"
 
         label = {
-            0: "neutral",
-            1: "entailed",
+            0: "not paraphrase",
+            1: "paraphrase",
         }.get(data['label_ids'].numpy())
 
 
